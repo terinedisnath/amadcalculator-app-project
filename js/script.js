e7531ddd9db366dd245a32c72bd84dc972316157
Terine Samarajeewa <terinedisnath@gmail.com>
2023-06-04 21:27:58 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var Number01, Number02, Answer, Oparators;
+
+
+
+function entNumber(num){
+
+    num = document.getElementById(txtscreen).value;
+
+    console.log(num)
+}
+
+
+function setoparators(op){
+    
+    console.log(op);
+}
